6564826c2b4f3c1bfe24bef4f5b1919af3e02932
jireh018 <jirehtuto71@gmail.com>
2021-04-16 11:29:32 -0300

    Redux folder done

---
+++ components/Main.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+export class Main extends Component {
+    componentDidMount()
+    {
+        
+    }
+   render(){
+        return (
+            <div>
+                
+            </div>
+        )
+   }
+}
